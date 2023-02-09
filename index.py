edd6f04f0a693950158fea291d51fca4af7d449f
Hhosnara <hhosnara17@gmail.com>
2023-02-09 13:49:11 +0600

    second  commit

---
+++ index.py
@@ -0,0 +1,15 @@
+#Learning python
+"""
+A new journey begins the moment you decide
+to start a new chapter in your life
+"""
+if 6>4:
+  print("six is greater than four");
+
+x = 5
+y = "Anha"  
+print(type(x))
+print(y)
+
+
+
