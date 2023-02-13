58ba2c5dd4a01d7a90536a3df4f68655c55e5286
Hhosnara <hhosnara17@gmail.com>
2023-02-13 23:05:36 +0600

    [lists learned]

---
+++ lists.py
@@ -0,0 +1,30 @@
+# lists
+
+# change item value
+list1 = ['apple', 'banana', 'cherry', 'orange', 'nuts']
+list1[1:3]= ['guava', 'melon','coconut']
+print(len(list1))
+
+# insert item 
+list1.insert(7, 'pineapple')
+print(list1)
+
+# append items
+
+list2 = ["pink", "yellow", "blue", "green", "red"]
+list2.append("orange")
+print(list2)
+
+# extend list
+
+list3 = ["all", "color", "names"]
+list2.extend(list3)
+print(list2)
+ 
+ #remove items
+list3.remove("all")
+print(list3)
+
+
+
+
