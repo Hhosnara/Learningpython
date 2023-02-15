1358b99c3d7b769c69a060763767be8c1c2e0ddb
Hhosnara <hhosnara17@gmail.com>
2023-02-15 10:32:58 +0600

    basic loops

---
+++ loop.py
@@ -0,0 +1,20 @@
+# while loop
+
+i = 1
+while i < 5:
+  print(i)
+  i += 1
+
+j = 1
+while j < 4:
+   print(j)
+   j += 1   
+
+#for  loop 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+  
